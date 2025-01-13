57e3acfbbe6bf56f07322e4414d5943547ce7eb0
MahabirGupta <mahabirg81@gmail.com>
2025-01-13 15:45:52 +0800

    Submit Todo Items

--- todoApp/src/components/Todo.jsx
+++ todoApp/src/components/Todo.jsx
@@ -2,10 +2,19 @@ import { useState } from "react";
 
 export default function Todo() {
   const [todo, setTodo] = useState(""); //initial value of todo is an empty string
+  // create a state to saved multiple todos
+  const [todos, setTodos] = useState([]); //store the todos as a list of array
+  function handleSubmit(e) {
+    e.preventDefault();
+    // spread operator ...todos
+    setTodos([...todos, todo]);
+    setTodo(""); // to reset the input field
+    // setTodos(todos.push);
+  }
   return (
     <div>
       {/* Create a form element */}
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <input
           onChange={(e) => setTodo(e.target.value)}
           value={todo}
@@ -13,6 +22,7 @@ export default function Todo() {
         />
         <button type="submit">Add</button>
       </form>
+      {console.log(todos)}
     </div>
   );
 }
